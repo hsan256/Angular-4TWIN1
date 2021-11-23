191d6acad90ab943cd42af12d178179c4d624561
medbouslah55 <47637052+medbouslah55@users.noreply.github.com>
2021-11-24 00:30:02 +0100

    Integration template V2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TestComponent } from './test/test.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home", component:TestComponent},
+  {path:"", redirectTo:"home", pathMatch:"full"},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
