b9d7430bb3e32489a7213ba60bd3dc3d13addb76
FirasKhadraoui <firas.khadraoui@esprit.tn>
2021-11-27 21:37:05 +0100

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListClientComponent } from './list-client/list-client.component';
 import { TestComponent } from './test/test.component';
 
 const routes: Routes = [
   {path:"home", component:TestComponent},
+  {path:"listclient", component:ListClientComponent},
   {path:"", redirectTo:"home", pathMatch:"full"},
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { TestComponent } from './test/test.component';
+import { ListClientComponent } from './list-client/list-client.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    TestComponent
+    TestComponent,
+    ListClientComponent
   ],
   imports: [
     BrowserModule,
